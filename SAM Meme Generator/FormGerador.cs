80bd1cab53051577c331185a3ca36be31a0d5d7b
Sávio <savioosiqueira@outlook.com>
2018-09-30 01:40:18 -0300

    otimizado o código

--- SAM Meme Generator/FormGerador.cs
+++ SAM Meme Generator/FormGerador.cs
@@ -1,14 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Drawing.Text;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using SAM_Meme_Generator.Properties;
 
@@ -27,11 +18,6 @@ namespace SAM_Meme_Generator
         }
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
         {
 
         }
